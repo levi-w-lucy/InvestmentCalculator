18e5ac88c7a01102a363a9e4857b610b2aa41919
levi-w-lucy <reachfan169@gmail.com>
2024-04-21 20:48:27 -0500

    Investment Calculator with hard coded values

--- investment_calculator.go
+++ investment_calculator.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"math"
+)
 
+func main() {
+	const inflationRate = 2.5
+	investmentAmount := 1000.0
+	years := 10.0
+	expectedReturnRate := 5.5
+
+	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+	fmt.Printf("$%.2f", futureValue)
+	fmt.Printf("\n$%.2f", futureRealValue)
 }
