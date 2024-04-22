3a6ffaf29ed38403343e8c0170791d1b71cf47d2
levi-w-lucy <reachfan169@gmail.com>
2024-04-21 21:01:41 -0500

    Add ability for User Input

--- investment_calculator.go
+++ investment_calculator.go
@@ -7,12 +7,21 @@ import (
 
 func main() {
 	const inflationRate = 2.5
-	investmentAmount := 1000.0
-	years := 10.0
-	expectedReturnRate := 5.5
+	var investmentAmount float64
+	var years float64
+	var expectedReturnRate float64
+
+	retreiveValFromUser("Enter an Investment Amount: ", &investmentAmount)
+	retreiveValFromUser("Enter Years to be invested: ", &years)
+	retreiveValFromUser("Enter Expected Rate of Return: ", &expectedReturnRate)
 
 	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
 	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
 	fmt.Printf("$%.2f", futureValue)
 	fmt.Printf("\n$%.2f", futureRealValue)
+}
+
+func retreiveValFromUser(message string, variablePtr *float64) {
+	fmt.Print(message)
+	fmt.Scan(variablePtr)
 }
